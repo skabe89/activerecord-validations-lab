dd3eaa41127d4ce23c4bca043a4bd56984859091
Sean <skabe89@gmail.com>
2021-01-16 12:44:23 -0500

    commit tree 0c1f4dd5de73886f8e6d240decbe0101532ca799

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, acceptance: {accept: ["Fiction", "Non-Fiction"]}
+  validate :click_baity
+
+  def click_baity
+    bait = ["Won't Believe", "Secret", "Guess", "Top [number]"]
+    if title && bait.none? {|b| title.match(b)}
+      errors.add(:title, "not click-baity enough!!!")
+    end
+  end
 end
